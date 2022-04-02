460d2d35a9b5d6855f9fb7074c8db5d77cb97d81
devicepc <69884110+devicepc@users.noreply.github.com>
2022-04-01 23:14:41 -0300

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,11 @@
+<?php
+ $destino= "devicepc01@gmail.com";
+ $nombre = $_POST["nombre"];
+ $correo = $_POST["correo"];
+ $mensaje = $_POST["mensaje"];
+ $contenido = "Nombre: ".$nombre ."\nCorreo: " . $correo . "\nMensaje :" . $mensaje;
+ mail($destino, "Contacto", $contenido);
+ header("Location:index.html");
+
+
+?>
